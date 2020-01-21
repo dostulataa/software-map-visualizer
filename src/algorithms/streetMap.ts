4bc42518c4c1801f91842c344f6fb64ed61aaf33
Andreas Blunk <andreas.blunk@maibornwolff.de>
2020-01-21 17:43:18 +0100

    review

--- src/algorithms/streetMap.ts
+++ src/algorithms/streetMap.ts
@@ -12,6 +12,7 @@ import Box from "../models/streetMap/Box";
 export default function init(root: CCNode, metric: string): HorizontalStreet {
     const boxes = createBoxes(root, 0);
     const street = new HorizontalStreet(root, boxes);
+    street.COLOR = 'gray'
     street.layout(metric);
     return street;
 }
@@ -21,13 +22,18 @@ export default function init(root: CCNode, metric: string): HorizontalStreet {
  * @param node current to create a box for
  * @param streetOrientation a child's street orientation: 0 for vertical, 1 for horizontal
  */
+// REVIEW: Ich würde für streetOrientation enum verwenden.
+// Dann entfällt die Codierung und es ist leicht ersichtlich was die Werte bedeuten.
 function createBoxes(node: CCNode, streetOrientation: number): Box[] {
     let children: Box[] = [];
     for (const child of node.children) {
         if(child.type === "File") {
             children.push(new House(child));
         } else {
-            children.push(streetOrientation === 0 ? new VerticalStreet(child, createBoxes(child, 1 - streetOrientation)) : new HorizontalStreet(child, createBoxes(child, 1 - streetOrientation)));
+            // REVIEW: Diese Zeile würde ich umbrechen. Sie ist zu lang.
+            children.push(streetOrientation === 0
+                ? new VerticalStreet(child, createBoxes(child, 1 - streetOrientation))
+                : new HorizontalStreet(child, createBoxes(child, 1 - streetOrientation)));
         }
     }
     return children;
--- src/models/streetMap/Box.ts
+++ src/models/streetMap/Box.ts
@@ -2,6 +2,7 @@ import CCNode from "../codeCharta/CCNode";
 import Point from "../visualization/Point";
 import VisualizationNode from "../visualization/VisualizationNode";
 
+// REVIEW: ich würde hier erklären was eine Box ist.
 export default abstract class Box {
     public node: CCNode;
     public height: number = 0;
@@ -14,11 +15,15 @@ export default abstract class Box {
      * Calculates height and width of the box.
      * @param metric 
      */
+    // REVIEW: layout ist eigentlich mehr als width und height-Berechnung.
+    // Dazu gehört auch die Positionierung von Knoten.
+    // Deshalb würde ich die Funktion eher calculateDimension() oder so nennen
     public abstract layout(metric: string): void;
 
     /**
      * Creates VisualizationNode for the box.
      * @param origin 
      */
+    // REVIEW: this is the actual layout()
     public abstract draw(origin: Point): VisualizationNode[];
 }
