856864068e8d9e74ac5931547a05dee38d22de35
Lukas Richter <lukas.richter@maibornwolff.de>
2020-01-27 16:02:52 +0100

    refactor, fixed cross highlighting, added tooltips

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import validateInputFiles from "./Validation";
 import Rectangle from "./models/visualization/Rectangle";
 import VisualNode from "./models/visualization/VisualNode";
 import CCProject from "./models/codeCharta/CCProject";
-import junit2018 from "./input/codecharta_2020-01-23_08-15.cc";
-import junit2019 from "./input/junit5_2019-10-26.cc";
+import junit2018 from "./input/codeCharta";
+import junit2019 from "./input/codecharta_2020-01-23_08-15.cc";
 import schema from "./schema";
 import { select, event } from "d3-selection";
 import squarify from "./algorithms/treemap/squarified";
 import Point from "./models/visualization/Point";
 import streetMap from "./algorithms/streetMap";
-// import strip from "./algorithms/strip";
 
 const inputFiles = [junit2018, junit2019];
 validateInputFiles(schema, inputFiles); // Checks for input data validity with schema
@@ -19,14 +18,14 @@ let svgHeight = 400;
 const projects = inputFiles.map(input => CCProject.create(input)); // Create projects for input files
 const metric = "rloc";
 
-// createVisualization(projects[0], strip, metric, 1, "oldVersion");
 createVisualization(projects[0], streetMap, metric, 0.5, "oldVersion");
+createVisualization(projects[1], streetMap, metric, 0.5, "newVersion");
 
 
 /**
- * Creates the Treemap using d3 for drawing
+ * Creates the Visualization using d3 for drawing
  * @param project the project that should be visualized
- * @param algorithm the treemap algorithm to be used
+ * @param algorithm the algorithm to be used
  * @param metric the metric to determine a nodes size
  * @param leafMargin margin for leaf nodes to make underlying nodes visible
  * @param versionId id of the version column
@@ -57,46 +56,48 @@ function createVisualization(project: CCProject, algorithm: Function, metric: st
         .data(nodes)
         .enter()
         .append('rect')
-        .attr("class", "treemapNode")
-        .attr("id", (d: VisualNode): string => { return versionId + "-" + d.node.name.replace(".", "-") })
-        .attr("x", (d: VisualNode): number => { return d.rectangle.topLeft.x + (isFile(d) ? leafMargin : 0) })
-        .attr("y", (d: VisualNode): number => { return d.rectangle.topLeft.y + (isFile(d) ? leafMargin : 0) })
-        .attr("height", (d: VisualNode): number => { return d.rectangle.height - (isFile(d) && d.rectangle.height > 2 * leafMargin ? 2 * leafMargin : 0) })
-        .attr("width", (d: VisualNode): number => { return d.rectangle.width - (isFile(d) && d.rectangle.width > 2 * leafMargin ? 2 * leafMargin : 0) })
+        .attr("class", "visualNode")
+        .attr("id", (d: VisualNode): string => { return (versionId + "-" + d.node.name).replace(/\.|\//g, "-") })
+        .attr("x", (d: VisualNode): number => { return d.rectangle.topLeft.x + (d.isFile() ? leafMargin : 0) })
+        .attr("y", (d: VisualNode): number => { return d.rectangle.topLeft.y + (d.isFile() ? leafMargin : 0) })
+        .attr("height", (d: VisualNode): number => { return d.rectangle.height - (d.isFile() && d.rectangle.height > 2 * leafMargin ? 2 * leafMargin : 0) })
+        .attr("width", (d: VisualNode): number => { return d.rectangle.width - (d.isFile() && d.rectangle.width > 2 * leafMargin ? 2 * leafMargin : 0) })
         .style("fill", (d: VisualNode): string => { return d.color })
         .on("mouseover", handleMouseover)
         .on("mouseout", handleMouseout)
-        .on("click", createAttributeList);
+        .on("click", createAttributeList)
+        .append("svg:title").text((d: VisualNode): string => { return `${d.node.name}\n${metric}: ${d.node.size(metric)}`});
 }
 
 /**
- * Handles mouse going inside of a treemap node. Changes color to highlighting color.
+ * Handles mouse going inside of a node. Changes color to highlighting color.
  * 
- * @param treemapNode treemap node which event is registered on 
+ * @param visualNode node which event is registered on 
  */
-function handleMouseover(treemapNode: VisualNode) {
+function handleMouseover(visualNode: VisualNode) {
     let color = "lightgrey";
     select(event.currentTarget).style("fill", color);
     const codeVersion = select(event.currentTarget.closest(".codeVersion"));
     const codeVersionId = codeVersion.attr("id");
-    colorizeOtherNode(codeVersionId, treemapNode.node.name.replace(".", "-"), color); // dot notation would imply a class
+    colorizeOtherNode(codeVersionId, visualNode.node.name.replace(/\.|\//g, "-"), color); // dot notation would imply a css class
 }
 
 /**
- * Handles mouse going out of a treemap node. Reverts color back to original color.
+ * Handles mouse going out of a node. Reverts color back to original color.
  * 
- * @param treemapNode treemap node which event is registered on 
+ * @param visualNode node which event is registered on 
  */
-function handleMouseout(treemapNode: VisualNode) {
-    let color = treemapNode.color;
-    select(event.currentTarget).style("fill", color);
+function handleMouseout(visualNode: VisualNode) {
+    const color = visualNode.color;
+    const target = select(event.currentTarget);
+    target.style("fill", color);
     const codeVersion = select(event.currentTarget.closest(".codeVersion"));
     const codeVersionId = codeVersion.attr("id");
-    colorizeOtherNode(codeVersionId, treemapNode.node.name.replace(".", "-"), color); // dot notation would imply a class
+    colorizeOtherNode(codeVersionId, visualNode.node.name.replace(/\.|\//g, "-"), color); // dot notation would imply a class
 }
 
 /**
- * colorizes the node with same name (if it exists) in other treemap version
+ * colorizes the node with same name (if it exists) in other visualization version
  * 
  * @param codeVersionId the code version id where event node lies 
  * @param nodeId id of event node
@@ -118,9 +119,9 @@ function colorizeOtherNode(codeVersionId: string, nodeId: string, color: string)
  * 
  * Creates the attribute table for a box
  * 
- * @param treemapNode the selected treemapNode
+ * @param visualNode the selected Node
  */
-function createAttributeList(treemapNode: VisualNode) {
+function createAttributeList(visualNode: VisualNode) {
     const codeVersion = select(event.currentTarget.closest(".codeVersion"));
     const attributeList = codeVersion.select(".attributes");
     if (!attributeList.select("table").empty()) {
@@ -132,9 +133,9 @@ function createAttributeList(treemapNode: VisualNode) {
     const headerCellRight = headerRow.append("th");
 
     headerCellLeft.text("name");
-    headerCellRight.text(treemapNode.node.name);
+    headerCellRight.text(visualNode.node.name);
 
-    treemapNode.node.attributes.forEach((value: number, key: string) => {
+    visualNode.node.attributes.forEach((value: number, key: string) => {
         const row = table.append("tr");
         const td = row.append("td").text(key);
         td.style("cursor", "pointer");
@@ -142,19 +143,10 @@ function createAttributeList(treemapNode: VisualNode) {
         td.on("mouseout", () => { select(event.currentTarget).style("background", "white") });
         td.on("click", () => {
             codeVersion.select("svg").remove();
-            let versionId = codeVersion.attr("id");
-            let project = versionId === "oldVersion" ? projects[0] : projects[1];
+            const versionId = codeVersion.attr("id");
+            const project = versionId === "oldVersion" ? projects[0] : projects[1];
             createVisualization(project, squarify, metric, 1, codeVersion.attr("id"));
         });
         row.append("td").text(String(value));
     });
-}
-
-/**
- * Checks if treemap node is of type "File"
- * 
- * @param treemapNode treemap node to be checked
- */
-function isFile(treemapNode: VisualNode): boolean {
-    return treemapNode.node.type === "File";
 }
