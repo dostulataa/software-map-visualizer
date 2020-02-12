28a660c8cf680bf2d25888c2785d49884d95c428
Lukas Richter <lukas.richter@maibornwolff.de>
2020-02-12 14:45:49 +0100

    added example file

---
+++ src/input/example2_flat.ts
@@ -0,0 +1,78 @@
+export default {
+	"projectName": "Example Project 2",
+	"apiVersion": "1.0",
+	"nodes": [
+		{
+			"name": "root",
+			"type": "Folder",
+			"attributes": {},
+			"children": [
+                {
+                    "name": "firstNode",
+					"type": "File",
+					"attributes": {
+                        "rloc": 600
+					},
+                    "children": [],
+					"link": "http://www.google.de"
+                },
+                {
+                    "name": "secondNode",
+					"type": "File",
+					"attributes": {
+                        "rloc": 400
+					},
+                    "children": [],
+					"link": "http://www.google.de"
+                },
+                {
+                    "name": "thirdNode",
+                    "type": "File",
+                    "attributes": {
+                        "rloc": 200
+                    },
+                    "children": [],
+                    "link": "http://www.google.de"
+                },
+                {
+                    "name": "fourthNode",
+                    "type": "File",
+                    "attributes": {
+                        "rloc": 600
+                    },
+                    "children": [],
+                    "link": "http://www.google.de"
+                },
+                {
+                    "name": "fifthNode",
+                    "type": "File",
+                    "attributes": {
+                        "rloc": 200
+                    },
+                    "children": [],
+                    "link": "http://www.google.de"
+                },
+                {
+                    "name": "sixthNode",
+					"type": "File",
+					"attributes": {
+						"rloc": 300
+					},
+                    "children": [],
+					"link": "http://www.google.de"
+                },
+                {
+					"name": "seventhNode",
+					"type": "File",
+					"attributes": {
+						"rloc": 100
+					},
+                    "children": [],
+					"link": "http://www.google.de"
+				},
+			]
+		}
+	],
+	"edges": [],
+	"attributeTypes": {}
+}
